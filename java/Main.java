f4f2d724130190eca6a044fe6af96770236c8e9f
Shreksatronik <90185952+Shreksatronik@users.noreply.github.com>
2022-10-01 15:18:40 +0700

    Delete java directory

--- java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Printer printer = new Printer();
-        new Thread2(printer);
-        for (int i = 1; i <= 5; i++) {
-            printer.printMain(i);
-        }
-    }
-}
