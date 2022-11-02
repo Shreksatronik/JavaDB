7f675b2d06bcbcd0d1ad8c173adc0318680b0b27
Shreksatronik <90185952+Shreksatronik@users.noreply.github.com>
2022-11-02 20:15:11 +0700

    task 2 Clojure

---
+++ task2/src/task2.clj
@@ -0,0 +1,17 @@
+(ns task2
+  (:require [clojure.test :as test]))
+
+(defn
+  eratosthenes_sieve [seq]
+  (cons (first seq) (lazy-seq (eratosthenes_sieve (filter #(not= 0 (mod % (first seq))) (rest seq))))))
+
+(test/deftest task2-test
+  (test/testing "Testing eratosthenes_sieve"
+    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 0) 2))
+    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 1) 3))
+    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 999) 7919))
+    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 1999) 17389))))
+
+(test/run-tests)
+
+
