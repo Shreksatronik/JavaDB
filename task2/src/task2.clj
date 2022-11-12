6d405e34f2bb1e648e616c8cc226f1ae23033a7c
Shreksatronik <90185952+Shreksatronik@users.noreply.github.com>
2022-11-12 15:38:37 +0700

    Update task2.clj

--- task2/src/task2.clj
+++ task2/src/task2.clj
@@ -1,16 +1,21 @@
 (ns task2
   (:require [clojure.test :as test]))
 
-(defn
-  eratosthenes_sieve [seq]
-  (cons (first seq) (lazy-seq (eratosthenes_sieve (filter #(not= 0 (mod % (first seq))) (rest seq))))))
+(defn eratosthenes_sieve [seq]
+  (cons
+    (first seq)
+    (lazy-seq (eratosthenes_sieve(filter #(not= 0 (mod % (first seq)))(rest seq))))))
+(def primes (eratosthenes_sieve (iterate inc 2)))
+
 
 (test/deftest task2-test
   (test/testing "Testing eratosthenes_sieve"
-    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 0) 2))
-    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 1) 3))
-    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 999) 7919))
-    (test/is (= (nth (eratosthenes_sieve (iterate inc 2)) 1999) 17389))))
+    (test/is (= (nth primes 0) 2))
+    (test/is (= (nth primes 1) 3))
+    (test/is (= (nth primes 999) 7919))
+    (test/is (= (nth primes 1999) 17389))
+    (test/is (<= (time (nth primes 1999)) (time (nth primes 1999))))))
+
 
 (test/run-tests)
 
