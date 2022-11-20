804a5c25a1d1c184e772bd6c3022cc507e10cd4b
Shreksatronik <90185952+Shreksatronik@users.noreply.github.com>
2022-11-20 18:49:39 +0700

    Update clj.clj

--- task3.clj/src/task3/clj.clj
+++ task3.clj/src/task3/clj.clj
@@ -10,18 +10,26 @@
            (doall) (map deref) (flatten)))
     (lazy-seq (handle-chunk (rest chunks) filter-f))))
 
-(defn p-filter-task3 ([filter-f sequence]
-   (handle-chunk (partition-all 1000 sequence) filter-f)))
+(defn p-filter-task3
+  [filter-f sequence]
+   (handle-chunk (partition-all 1000 sequence) filter-f))
 
 (defn heavy_even?
-  [arg] (Thread/sleep 1) (even? arg));
+  [arg]
+  (Thread/sleep 1) (even? arg))
 
 (time (nth (p-filter-task3 heavy_even? (range)) 1000))
 (time (nth (filter heavy_even? (range)) 1000))
 
+(defn exep
+  [el1, el2]
+  (if (>= el2 el1) ()
+  (println(nth (p-filter-task3 any? (range el1))el2))
+  ))
 
 (test/deftest test-adder
   (test/testing "Testing parallel filter"
+    (test/is (=(exep 10 10)()))
     (test/is (=  '(4 9 14 19 24 29 34 39 44 49) (take 10 (p-filter-task3 #(= 4 (mod % 5)) (range)))))
     (test/is (=  80 (nth (p-filter-task3 heavy_even? (range 1000000)) 40)))
     (test/is (= '(0 2 4 6 8 10 12 14 16 18) (take 10 (p-filter-task3 heavy_even? (range)))))
