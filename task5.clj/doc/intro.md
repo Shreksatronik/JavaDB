4fccfddda08393c243c3d3f8993612f8f375fb02
Shreksatronik <90185952+Shreksatronik@users.noreply.github.com>
2022-12-04 00:27:49 +0700

    Add files via upload

---
+++ task5.clj/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to task5.clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ task5.clj/test/task5/clj_test.clj
@@ -0,0 +1,7 @@
+(ns task5.clj-test
+  (:require [clojure.test :refer :all]
+            [task5.clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
